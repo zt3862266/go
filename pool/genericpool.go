34f4f0bc95bc89eaee82634c1b8538f9596d0f60
tao <zt3862266@gmail.com>
2019-03-29 16:40:43 +0800

    when close ,wait for 5 second

--- pool/genericpool.go
+++ pool/genericpool.go
@@ -1,137 +1,139 @@
 package genericpool
 
 import (
-	"time"
-	"sync"
 	"errors"
+	"sync"
+	"time"
 
 	"github.com/zt3862266/go/log"
 )
 
-var(
-	ErrWrongParam = errors.New("pool config invalid")
-	ErrConnFailed = errors.New("Connection failed")
-	ErrChanClosed = errors.New("channel is closed")
+var (
+	ErrWrongParam  = errors.New("pool config invalid")
+	ErrConnFailed  = errors.New("Connection failed")
+	ErrChanClosed  = errors.New("channel is closed")
 	ErrIdleTimeOut = errors.New("exceed max idle time")
-	ErrNilConn = errors.New("connection is nil,reject")
+	ErrNilConn     = errors.New("connection is nil,reject")
 )
 
 // this is connection pool struct
-type Pool struct{
-	InitSize int
-	MaxSize int
+type Pool struct {
+	InitSize    int
+	MaxSize     int
 	MaxIdleTime time.Duration
-	Factory func() (Conn,error)
-	lock *sync.Mutex
-	PoolChan chan connWrapper
-
+	Factory     func() (Conn, error)
+	lock        *sync.Mutex
+	PoolChan    chan connWrapper
 }
 
 // conn struct
-type Conn interface{
+type Conn interface {
 	Close() error
 }
 
 // connection的一层wrapper,增加了连接创造时间
-type connWrapper struct{
+type connWrapper struct {
 	CreateTime time.Time
 	Conn
 }
 
-
-func NewPool(factory func() (Conn,error) ,initSize int,maxSize int,maxIdleTime time.Duration) (pool *Pool,err error){
+func NewPool(factory func() (Conn, error), initSize int, maxSize int, maxIdleTime time.Duration) (pool *Pool, err error) {
 
 	if initSize < 1 || maxSize < 1 || initSize > maxSize {
-		return nil,ErrWrongParam
+		return nil, ErrWrongParam
 	}
 	pool = &Pool{
-		InitSize:initSize,
-		MaxSize:maxSize,
-		MaxIdleTime:maxIdleTime,
-		lock:  &sync.Mutex{},
-		PoolChan:make(chan connWrapper,maxSize),
-		Factory:factory,
+		InitSize:    initSize,
+		MaxSize:     maxSize,
+		MaxIdleTime: maxIdleTime,
+		lock:        &sync.Mutex{},
+		PoolChan:    make(chan connWrapper, maxSize),
+		Factory:     factory,
 	}
-	for i:=0;i<initSize;i++{
-		conn ,err := factory()
-		if err !=nil{
-			return nil,ErrConnFailed
+	for i := 0; i < initSize; i++ {
+		conn, err := factory()
+		if err != nil {
+			return nil, ErrConnFailed
 		}
-		pool.PoolChan <-  connWrapper{
-			Conn:conn,
-			CreateTime:time.Now(),
+		pool.PoolChan <- connWrapper{
+			Conn:       conn,
+			CreateTime: time.Now(),
 		}
 	}
-	go func(pool *Pool){
+	go func(pool *Pool) {
 
-		for{
-			log.Info("pool InitSize:%d,MaxSize:%d,poolLen:%d",pool.InitSize,pool.MaxSize,len(pool.PoolChan))
-			time.Sleep(time.Second *5)
+		for {
+			log.Info("pool InitSize:%d,MaxSize:%d,poolLen:%d", pool.InitSize, pool.MaxSize, len(pool.PoolChan))
+			time.Sleep(time.Second * 5)
 		}
 
 	}(pool)
-	return pool,nil
+	return pool, nil
 }
 
+func (p *Pool) Get() (conn Conn, err error) {
 
-func (p *Pool) Get() (conn Conn, err error){
-
-	f  := func() {
+	f := func() {
 		p.lock.Lock()
 		defer p.lock.Unlock()
-		if len(p.PoolChan) == 0  {
+		if len(p.PoolChan) == 0 {
 			conn, err := p.Factory()
 			if err != nil {
 				return
 			}
 			p.PoolChan <- connWrapper{
-				Conn:conn,
-				CreateTime:time.Now(),
+				Conn:       conn,
+				CreateTime: time.Now(),
 			}
 		}
 	}
-	for{
-		select{
-			case connWrapper,ok := <- p.PoolChan:
-				if ok {
-					if connWrapper.CreateTime.Add(p.MaxIdleTime).Before(time.Now()) {
-						connWrapper.Conn.Close()
-						log.Info("conn idle timeout:%v",ErrIdleTimeOut)
-						continue
-					}else {
-						return connWrapper.Conn, nil
-					}
-				}else{
-					return nil,ErrChanClosed
+	for {
+		select {
+		case connWrapper, ok := <-p.PoolChan:
+			if ok {
+				if connWrapper.CreateTime.Add(p.MaxIdleTime).Before(time.Now()) {
+					connWrapper.Conn.Close()
+					log.Info("conn idle timeout:%v", ErrIdleTimeOut)
+					continue
+				} else {
+					return connWrapper.Conn, nil
 				}
-			default:
-				f()
+			} else {
+				return nil, ErrChanClosed
+			}
+		default:
+			f()
 		}
 
 	}
 }
 
-func (p *Pool) Release(conn Conn) error{
-	if conn ==nil{
+func (p *Pool) Release(conn Conn) error {
+	if conn == nil {
 		return ErrNilConn
 	}
 
 	p.lock.Lock()
-	select{
-		case p.PoolChan <- connWrapper{Conn:conn, CreateTime:time.Now(),}:
-			p.lock.Unlock()
-			return nil
-		default:
-			p.lock.Unlock()
-			log.Info("conn chan full,close")
-			return conn.Close()
+	select {
+	case p.PoolChan <- connWrapper{Conn: conn, CreateTime: time.Now()}:
+		p.lock.Unlock()
+		return nil
+	default:
+		p.lock.Unlock()
+		log.Info("conn chan full,close")
+		go func() {
+
+			time.Sleep(time.Second * 5)
+			conn.Close()
+		}()
+		return nil
 
 	}
 	return nil
 }
 
-func (p *Pool) close(){
-	for connWrapper := range p.PoolChan{
+func (p *Pool) close() {
+	for connWrapper := range p.PoolChan {
 		connWrapper.Conn.Close()
 	}
 	p.Factory = nil
